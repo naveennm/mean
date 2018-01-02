67d042383fa9105bcaa59aecaf57c9cb7026d165
naveennm <naveen_m@live.com>
2018-01-02 13:46:46 +0530

    db

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
     // configuration =================
 
-    mongoose.connect('mongodb://localhost:27017/test');     // connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://mean:mean@ds135537.mlab.com:35537/test123');     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
